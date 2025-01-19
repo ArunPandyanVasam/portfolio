5f0c479e1222a069da55de311ee493293022acbe
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-19 12:39:33 -0500

    imported App.module.css in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
+import styles from'./App.module.css'
 
 function App() {
 
   return (
-    <>
-      Hello world
-    </>
+    <div>
+      Hello world!
+    </div>
   )
 }
 
