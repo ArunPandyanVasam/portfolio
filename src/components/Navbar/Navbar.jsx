c6dd2ea0bef284252a5f32967fe820d43f1282be
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-30 17:08:24 -0500

    Menubar icon added.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   return (
@@ -8,6 +10,7 @@ export const Navbar = () => {
         Portfolio
       </a>
       <div className={styles.menu}>
+        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button"/>
         <ul className={styles.menuItems}>
           <li>
             <a href="#about">About</a>
