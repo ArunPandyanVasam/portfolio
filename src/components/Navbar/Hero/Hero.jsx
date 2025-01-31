072cac8faabe21324ad0ab53bd77fb3ee3b04b08
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-31 14:15:14 -0500

    css added to hero section

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -7,11 +7,13 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1>Hi, I'm Arun!</h1>
-        <p>I'm a entry level job seeker</p>
-        <a href="vasamarunpandyan@gmail.com">Contact Me</a>
+        <h1 className={styles.title}>Hi, I'm Arun!</h1>
+        <p className={styles.description}>I'm a entry level job seeker</p>
+        <a href="vasamarunpandyan@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image" />
+      <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image" className={styles.heroImg}/>
       <div className={StyleSheet.topBlur} />
       <div className={StyleSheet.bottomBlur} />
     </section>
