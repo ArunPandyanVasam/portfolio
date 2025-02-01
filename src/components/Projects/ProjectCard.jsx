931b43695f20d67c45c05f18b3a883c1c8225262
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-31 21:23:49 -0500

    Skills now have skills

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -11,20 +11,26 @@ export const ProjectCard = ({
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
-        className={styles.imahe}
+        className={styles.image}
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
         {skills.map((skill, id) => {
-          <li key={id} className={styles.skill}>
-            {skill}
-          </li>;
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>Demo</a>
-        <a href={source} className={styles.link}>Source</a>
+        <a href={demo} className={styles.link}>
+          Demo
+        </a>
+        <a href={source} className={styles.link}>
+          Source
+        </a>
       </div>
     </div>
   );
