8b30d8e813a9b493a9dd3e855df993c507b9c147
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-31 22:45:10 -0500

    font size is chand in the hero section for h1 and paragraph

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,13 +7,23 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Arun!</h1>
-        <p className={styles.description}>I'm a entry level job seeker</p>
+      <h1 className={styles.title}>Hello, I'm <br />Arun Pandyan</h1>
+        <p className={styles.description}>
+          An aspiring software developer with a diploma in Computer Programming.
+          Passionate about coding, problem-solving, and learning new
+          technologies. Skilled in Java, C, and Python, with hands-on experience
+          in web development and database management. Excited to grow and
+          contribute to real-world projects.
+        </p>
         <a href="vasamarunpandyan@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image" className={styles.heroImg}/>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero Image"
+        className={styles.heroImg}
+      />
       <div className={StyleSheet.topBlur} />
       <div className={StyleSheet.bottomBlur} />
     </section>
