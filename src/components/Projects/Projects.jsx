c484031db439e533f02d397a4f895017f5715adc
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-02-02 15:38:56 -0500

    Background color is changed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,7 +7,8 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>Recent Works</h2>
+      <p className={styles.subtitle}>Here are some of my favorite projects.<br />Feel free to check them out.</p>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
