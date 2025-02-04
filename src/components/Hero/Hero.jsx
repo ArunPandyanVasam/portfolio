bd5e21be4a65b098eca224cb4e86ba39c8aa0e9d
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-02-04 13:23:55 -0500

    Footer is changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,8 +8,6 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        {/* <h1 className={styles.title}> - HELLO
-        </h1> */}
         <p className={styles.description}>
           I am Arun, an entry-level
           <br />
@@ -19,20 +17,12 @@ export const Hero = () => {
           <br />
           to learn and contribute.
         </p>
-        {/* <a href={resume} download='resume' className={styles.contactBtn}>
-          Download Resume
-        </a> */}
         <a href="mailto:arunpandyanv@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
-      {/* <img
-        src={getImageUrl("hero/heroImage.png")}
-        alt="Hero Image"
-        className={styles.heroImg}
-      /> */}
-      <div className={StyleSheet.topBlur} />
-      <div className={StyleSheet.bottomBlur} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
