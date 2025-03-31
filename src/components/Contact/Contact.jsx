c45678f8dc52a5c0a527779761d8ba12416b18b2
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-03-31 18:01:09 -0400

    Added copywights in footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,30 +6,44 @@ export const Contact = () => {
   return (
     <footer className={styles.container} id="contact">
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contact Me</h2>
+        <p>If you have any questions, or just want to chat, feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
-          <a href="mailto:arunpandyanv@gmail.com" target="_blank">arunpandyanv@gmail.com</a>
+          <img 
+            src={getImageUrl("contact/emailIcon.png")} 
+            alt="Email Icon" 
+            aria-label="Email Icon"
+          />
+          <a href="mailto:arunpandyanv@gmail.com" target="_blank" rel="noopener noreferrer">
+            arunpandyanv@gmail.com
+          </a>
         </li>
         <li className={styles.link}>
-          <img
+          <img 
             src={getImageUrl("contact/linkedinIcon.png")}
-            alt="Linked Icon"
+            alt="LinkedIn Icon" 
+            aria-label="LinkedIn Icon"
           />
-          <a href="https://www.linkedin.com/in/arun-vasam/" target="_blank">
+          <a href="https://www.linkedin.com/in/arun-vasam/" target="_blank" rel="noopener noreferrer">
             www.linkedin.com/in/arun-vasam/
           </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
-          <a href="https://github.com/ArunPandyanVasam" target="_blank">
+          <img 
+            src={getImageUrl("contact/githubIcon.png")} 
+            alt="GitHub Icon" 
+            aria-label="GitHub Icon"
+          />
+          <a href="https://github.com/ArunPandyanVasam" target="_blank" rel="noopener noreferrer">
             github.com/ArunPandyanVasam
           </a>
         </li>
       </ul>
+      <div className={styles.footerBottom}>
+        <p>&copy; 2025 Arun Pandyan. All Rights Reserved.</p>
+      </div>
     </footer>
   );
 };
