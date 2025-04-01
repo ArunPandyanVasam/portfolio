7551a1878c0783f1a283ba70b030cb68c91c18ce
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-03-31 20:17:28 -0400

    Content change in footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,42 +7,41 @@ export const Contact = () => {
     <footer className={styles.container} id="contact">
       <div className={styles.text}>
         <h2>Contact Me</h2>
-        <p>If you have any questions, or just want to chat, feel free to reach out!</p>
+        <p>If you have any questions or would like to connect, feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img 
-            src={getImageUrl("contact/emailIcon.png")} 
-            alt="Email Icon" 
-            aria-label="Email Icon"
-          />
-          <a href="mailto:arunpandyanv@gmail.com" target="_blank" rel="noopener noreferrer">
-            arunpandyanv@gmail.com
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img 
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn Icon" 
-            aria-label="LinkedIn Icon"
-          />
-          <a href="https://www.linkedin.com/in/arun-vasam/" target="_blank" rel="noopener noreferrer">
-            www.linkedin.com/in/arun-vasam/
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img 
-            src={getImageUrl("contact/githubIcon.png")} 
-            alt="GitHub Icon" 
-            aria-label="GitHub Icon"
-          />
-          <a href="https://github.com/ArunPandyanVasam" target="_blank" rel="noopener noreferrer">
-            github.com/ArunPandyanVasam
-          </a>
-        </li>
-      </ul>
-      <div className={styles.footerBottom}>
-        <p>&copy; 2025 Arun Pandyan. All Rights Reserved.</p>
+      <div className={styles.linksContainer}>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/emailIcon.png")}
+              alt="Email Icon"
+              aria-label="Email Icon"
+            />
+            <a href="mailto:arunpandyanv@gmail.com" target="_blank" rel="noopener noreferrer">
+              arunpandyanv@gmail.com
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="LinkedIn Icon"
+              aria-label="LinkedIn Icon"
+            />
+            <a href="https://www.linkedin.com/in/arun-vasam/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="GitHub Icon"
+              aria-label="GitHub Icon"
+            />
+            <a href="https://github.com/ArunPandyanVasam" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
