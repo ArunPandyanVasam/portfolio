ccdb0f54ef86da444bd300b3e3da92d9f2298bfc
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-03-31 21:43:03 -0400

    Hero content changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,15 +6,12 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>
-          Hi there! Welcome to my digital space!
-        </h1>
+        <h1 className={styles.heading}>Hey! I’m Arun Pandyan Vasam</h1>
         <p className={styles.name}>
-          I am Arun Pandyan Vasam a passionate Software Developer with expertise
-          in Java, React, C++, and cloud technologies. Experienced in building
-          scalable applications, optimizing software performance, and
-          integrating real-time processing. Always eager to learn, innovate, and
-          contribute to impactful projects.
+          A Software Developer with a strong foundation in Java, React,
+          Python, and cloud technologies. I specialize in building scalable
+          applications, optimizing real-time processes, and solving complex
+          problems. Passionate about AI-driven solutions, and high-performance software development.
         </p>
 
         <a href="mailto:arunpandyanv@gmail.com" className={styles.contactBtn}>
